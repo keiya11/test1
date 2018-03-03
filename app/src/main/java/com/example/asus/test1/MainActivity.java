17fff15745977d53c196c2984104056ec36fe45d
Keiya <waridthefrance@gmail.com>
2018-03-03 19:01:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asus/test1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.asus.test1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button = (Button) findViewById(R.id.button);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void next(View view) {
+        startActivity(new Intent(MainActivity.this, Main2Activity.class));
+    }
+}
